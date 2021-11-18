fe27b2cc66ad9f74a9c0b7fb2f17a58fd9990bb2
MagomedChe <mdweb@bk.ru>
2021-11-18 15:44:20 +0300

    adfe

--- src/components/Messages/index.jsx
+++ src/components/Messages/index.jsx
@@ -1,19 +1,16 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { loadMessages } from "../../redux/ducks/messagesReduser";
-import CheckIcon from '@mui/icons-material/Check';
+import CheckIcon from "@mui/icons-material/Check";
 import SearchMessage from "./SearchMessage";
-import IconProfile from "./IconProfile"
-import {useParams} from "react-router-dom";
+import IconProfile from "./IconProfile";
+import { useParams } from "react-router-dom";
 
-
-
-function Messages({activeProfile, setActiveProfile}) {
+function Messages({ activeProfile, setActiveProfile }) {
   const messages = useSelector((state) => state.messages.messages);
   const dispatch = useDispatch();
   const params = useParams();
   const loading = useSelector((state) => state.messages.loading);
-
 
   useEffect(() => {
     dispatch(loadMessages());
@@ -22,37 +19,30 @@ function Messages({activeProfile, setActiveProfile}) {
   return (
     <div className="chat">
       <div className="chat-header">
-        <SearchMessage/>
+        <SearchMessage />
         <div className="chat-name">Имя автора</div>
-        <div className="iconProfile" onClick={() => setActiveProfile(!activeProfile)}>
-          <IconProfile/>
+        <div
+          className="iconProfile"
+          onClick={() => setActiveProfile(!activeProfile)}
+        >
+          <IconProfile />
         </div>
       </div>
       <div className="chat-messages">
-        <div className="message">
-          <div className="text">
-            {loading ? (<div>Идет загрузка ...</div>) : (
-                <div>
-                  {messages.map((message) => {
-                    return <div className="chat-content"> {message.content} </div>
-                  })}
-                </div>
-            )
-
-            }
+        {loading ? (
+          <div>Идет загрузка ...</div>
+        ) : (
+          <div className="chat-content">
+            {messages.map((message) => {
+              return <div className="chat-text"> {message.content} </div>;
+            })}
           </div>
-          <div className="time-message">
-              time
-          </div>
-          <CheckIcon fontSize={'small'}/>
-        </div>
-        </div>
+        )}
+        <div className="time-message">time</div>
+        <CheckIcon fontSize={"small"} />
+      </div>
     </div>
   );
 }
 
 export default Messages;
-
-
-
-
