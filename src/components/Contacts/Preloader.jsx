0a0517823c756a74dd6654b8a9a3f857469ab2b2
MagomedChe <mdweb@bk.ru>
2022-01-20 16:44:01 +0300

    20

---
+++ src/components/Contacts/Preloader.jsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import Grid from '@mui/material/Grid';
+import Skeleton from '@mui/material/Skeleton';
+
+function SkeletonChildrenDemo(props) {
+  const preloader = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  return preloader.map(() => {
+    return (
+      <div>
+        <Box sx={{ display: 'flex', alignItems: 'center' }} animation="wave">
+          <Box sx={{ margin: 1 }}>
+            <Skeleton variant="circular" animation="wave">
+              <Avatar />
+            </Skeleton>
+          </Box>
+          <Box sx={{ width: '220px' }}>
+            <Skeleton width="100%" height="50px" animation="wave"></Skeleton>
+            <Skeleton width="100%" animation="wave"></Skeleton>
+          </Box>
+        </Box>
+      </div>
+    );
+  });
+}
+
+SkeletonChildrenDemo.propTypes = {
+  loading: PropTypes.bool,
+};
+
+export default function SkeletonChildren() {
+  return (
+    <Grid container spacing={8}>
+      <Grid item xs>
+        <SkeletonChildrenDemo loading />
+      </Grid>
+      <Grid item xs></Grid>
+    </Grid>
+  );
+}
--- src/components/Contacts/index.jsx
+++ src/components/Contacts/index.jsx
@@ -4,7 +4,7 @@ import Contact from './Contact';
 import SearchContact from './SearchContact';
 import { loadContacts } from '../../redux/ducks/contactsReducer';
 import { loadMyId } from '../../redux/ducks/applicationReduser';
-
+import Preloader from './Preloader'
 function Contacts(props) {
   const dispatch = useDispatch();
   const loading = useSelector((state) => state.contacts.loading);
@@ -20,21 +20,20 @@ function Contacts(props) {
     dispatch(loadMyId());
   }, [dispatch]);
 
-  return (
+  return loading ? (
+      <Preloader/>
+  ) : (
     <div className="sidebar">
       <div className="search-contact">
         <SearchContact setSearchContact={setSearchContact}/>
       </div>
       <div className="contacts">
-        {loading ? (
-          <div>Идет загрузка ...</div>
-        ) : (
           <div>
             {filteredContacts.map((contact) => {
               return <Contact contact={contact} key={contact._id} />;
             })}
           </div>
-        )}
+
       </div>
     </div>
   );
